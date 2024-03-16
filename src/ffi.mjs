a9b0ce1793cf49a5f900bb7a9f82115c0a4b56e2
Josh Gillies <hey+github@josh.gl>
2024-03-16 12:52:39 +1100

    Remove esbuild from project

--- src/ffi.mjs
+++ src/ffi.mjs
@@ -1,13 +1,4 @@
 /**
- * Load the esbuild module
- * @returns {Promise<any>}
- */
-async function esbuild() {
-  const esbuild = await import("https://deno.land/x/esbuild@v0.20.1/mod.js");
-  return esbuild;
-}
-
-/**
  * Get the project name, resolveDir and entry point from the projects gleam.toml file
  * @returns {Promise<{projectName: string, resolveDir: string, entry: string}>}
  */
@@ -27,11 +18,11 @@ async function getProjectInfo() {
 /**
  * Find the source file that contains the given handler code
  * TODO: this is a naive implementation, it should be improved
+ * @param {string} resolveDir
  * @param {string} handlerCode
  * @returns {Promise<string[]>}
  */
-async function findHandlerSourceFile(handlerCode) {
-  const { resolveDir } = await getProjectInfo();
+async function findHandlerSourceFile(resolveDir, handlerCode) {
   const files = [];
   for await (const entry of Deno.readDir(resolveDir)) {
     if (entry.name.endsWith(".mjs")) {
@@ -51,58 +42,41 @@ async function findHandlerSourceFile(handlerCode) {
  */
 export async function create(handler) {
   const { resolveDir } = await getProjectInfo();
-  const { build } = await esbuild();
   const handlerCode = handler.toString();
   const handlerId = `__${Math.random().toString(36).substring(7)}`;
   // TODO: what if the handler is not found, or there are many files with the same handler?|
-  const [handlerFile] = await findHandlerSourceFile(handlerCode);
+  const [handlerFile] = await findHandlerSourceFile(resolveDir, handlerCode);
   const handlerSource = await Deno.readTextFile(`${resolveDir}/${handlerFile}`);
-  // this code is used by esbuild to resolve as much of the handler scope as possible
-  const contents = `${handlerSource};export const ${handlerId}=${handlerCode}`;
-  // this is the global name that the handler will be available under
-  const globalName = "nanoworker";
-
-  let bundle = await build({
-    stdin: {
-      contents,
-      resolveDir,
-    },
-    bundle: true,
-    write: false,
-    platform: "node",
-    format: "iife",
-    globalName,
-    target: "esnext",
-  });
 
   const workerCode = `
   // bundled code
-  ${bundle.outputFiles[0].text};
+  ${handlerSource};
   // handler code should be wrapped in a onmessage event listener
   onmessage = (e) => {
-      const result = ${globalName}.${handlerId}(e.data);
+      const result = (${handlerCode})(e.data);
       postMessage(result);
   };`;
 
-  const blob = new Blob([workerCode], { type: "application/javascript" });
-  const url = URL.createObjectURL(blob);
-  const worker = new Worker(url, { type: "module" });
-  const workerProxy = new Proxy(worker, {
-    get: (target, prop, receiver) => {
-      const value = target[prop];
-      if (value instanceof Function) {
-        if (prop === "terminate") {
-          URL.revokeObjectURL(url);
-        }
-        return function (...args) {
-          return value.apply(this === receiver ? target : this, args);
-        };
-      }
-      return value;
-    },
+  const workerFile = `${handlerFile.replace(
+    ".mjs",
+    `.worker.${handlerId}.mjs`
+  )}`;
+  const workerPath = `${resolveDir}/${workerFile}`;
+
+  console.log(workerPath);
+  await Deno.writeTextFile(workerPath, workerCode);
+
+  globalThis.addEventListener("unload", () => {
+    try {
+      Deno.removeSync(workerPath);
+    } catch (e) {}
   });
 
-  return workerProxy;
+  const worker = new Worker(import.meta.resolve(`./${workerFile}`), {
+    type: "module",
+  });
+
+  return worker;
 }
 
 /**
